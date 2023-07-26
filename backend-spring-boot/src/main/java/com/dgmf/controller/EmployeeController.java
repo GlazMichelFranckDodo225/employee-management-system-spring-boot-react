a40f124b0dfa62f71bbfcb5c94d950afd2f22cbf
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-07-26 06:58:42 +0200

    From the React Frontend and through the Spring Boot Backend, implementation of the Workflow for adding an "Employee" in the DB

--- backend-spring-boot/src/main/java/com/dgmf/controller/EmployeeController.java
+++ backend-spring-boot/src/main/java/com/dgmf/controller/EmployeeController.java
@@ -4,13 +4,11 @@ import com.dgmf.dto.EmployeeDTO;
 import com.dgmf.entity.Employee;
 import com.dgmf.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:3000/") /* Frontend Application Url */
 @RequiredArgsConstructor
 public class EmployeeController {
     private final EmployeeService employeeService;
--- frontend-react/src/components/AddEmployee.js
+++ frontend-react/src/components/AddEmployee.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import EmployeeService from '../services/EmployeeService';
 
 const AddEmployee = () => {
+    // Default of an Employeee
+    const [employee, setEmployee] = useState({
+        "id":"",
+        "firstName": "",
+        "lastName": "",
+        "emailId": ""
+    })
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        /* I have to set the Employee with the existing Employee */
+        setEmployee({...employee, [e.target.name]: value});
+    }
+
+    const saveEmployee = (e) => {
+        // Disable refreshing of the page
+        e.preventDefault();
+        // Calling the API Axios
+        EmployeeService.saveEmployee(employee).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
     return (
         <div className='flex max-w-2xl mx-auto shadow border-b'>
             <div className='px-8 py-8'>
@@ -10,25 +36,53 @@ const AddEmployee = () => {
                 {/* First Name */}
                 <div className='items-center justify-center h-14 w-full my-4'>
                     <label className='block text-gray-600 text-sm font-normal'>First Name</label>
-                    <input type="text" className='h-10 w-96 border mt-2 px-2 py-2'></input>
+                    <input 
+                    type="text" 
+                    className='h-10 w-96 border mt-2 px-2 py-2' 
+                    name='firstName'
+                    value={employee.firstName}
+                    onChange={(e) => handleChange(e)}
+                    ></input>
                 </div>
 
                 {/* Last Name */}
                 <div className='items-center justify-center h-14 w-full my-4'>
                     <label className='block text-gray-600 text-sm font-normal'>Last Name</label>
-                    <input type="text" className='h-10 w-96 border mt-2 px-2 py-2'></input>
+                    <input 
+                    type="text" 
+                    className='h-10 w-96 border mt-2 px-2 py-2'
+                    name='lastName'
+                    value={employee.lastName}
+                    onChange={(e) => handleChange(e)}
+                    ></input>
                 </div>
 
                 {/* Email */}
                 <div className='items-center justify-center h-14 w-full my-4'>
                     <label className='block text-gray-600 text-sm font-normal'>Email</label>
-                    <input type="email" className='h-10 w-96 border mt-2 px-2 py-2'></input>
+                    <input 
+                    type="email" 
+                    className='h-10 w-96 border mt-2 px-2 py-2'
+                    name='emailId'
+                    value={employee.emailId}
+                    onChange={(e) => handleChange(e)}
+                    ></input>
                 </div>
 
                 {/* Save | Clear */}
                 <div className='items-center justify-center h-14 w-full my-4 space-x-4 pt-4'>
-                    <button className='rounded text-white font-semibold bg-green-400 py-2 px-6 hover:bg-green-700'>Save</button>
-                    <button className='rounded text-white font-semibold bg-red-400 py-2 px-6 hover:bg-red-700'>Clear</button>
+                    <button 
+                    className='rounded text-white font-semibold bg-green-400 py-2 px-6 hover:bg-green-700'
+                    /* On clicking on the "save button":
+                        - the "saveEmployee()" function is called,
+                        - the "saveEmployee()" function call the "EmployeeService.saveEmployee()" function,
+                        - the "EmployeeService.saveEmployee()" function call "axios.post(EMPLOYEE_API_BASE_URL, employee)"
+                    */
+                    onClick={saveEmployee}
+                    >Save</button>
+                    <button 
+                    className='rounded text-white font-semibold bg-red-400 py-2 px-6 hover:bg-red-700'
+                    >Clear</button>
                 </div>
             </div>
         </div>
